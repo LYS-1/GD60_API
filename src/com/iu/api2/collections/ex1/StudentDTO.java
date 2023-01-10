9519f4769d4b06e33aa0eb1e19c92531cc6329d9
LYS-1 <skyduq123@gmail.com>
2023-01-10 13:32:38 +0900

    01_10

---
+++ src/com/iu/api2/collections/ex1/StudentDTO.java
@@ -0,0 +1,11 @@
+package com.iu.api2.collections.ex1;
+
+public class StudentDTO {
+	private String name;
+	private int number;
+	private int kor;
+	private int eng;
+	private int math;
+	private int sum;
+	private double avg;
+}
